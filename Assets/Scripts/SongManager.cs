43ed5ddbf9f53c818ba7502faf5a8ce4ce3d3165
VictorShu <vshu@ucsd.edu>
2024-02-09 12:15:53 -0800

    testtest

--- Assets/Scripts/SongManager.cs
+++ Assets/Scripts/SongManager.cs
@@ -15,7 +15,7 @@ public class SongManager : Singleton<SongManager>
     public static float deltaTime;
 
     public static event Action OnBeat;
-    // test commit
+    // test commit fhsjakfns
     public void Play(AudioClip audioClip)
     {
         audioSource.Stop();
