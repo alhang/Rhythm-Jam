e3d0ca4e4d2bad9ad847951db11bed8b4fa4561b
victor-shu <victor0shu@gmail.com>
2024-02-16 15:32:46 -0800

    edited main menu

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -8,6 +8,9 @@ public class MainMenu : MonoBehaviour
     [SerializeField] PlayerStatsSO playerStats;
 
     private string gameScenePath = "Assets/Scenes/GameScene.unity";
+    private string mainMenuScenePath = "Assets/Scenes/MainMenu.unity";
+    private string controlsScenePath = "Assets/Scenes/ControlsPage.unity";
+
 
     public void Play()
     {
@@ -18,6 +21,16 @@ public class MainMenu : MonoBehaviour
     public void Options()
     {
         
+    }
+
+    public void Controls()
+    {
+        SceneManager.LoadScene(controlsScenePath);
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene(mainMenuScenePath);
     }
 
     public void Quit()
